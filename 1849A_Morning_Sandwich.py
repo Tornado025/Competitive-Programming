ea07e7f309ef5fb8dec5bdcd81c4127de8cf61ba
Rishon Thomas Joby <rishonthomasjoby@gmail.com>
2025-02-01 16:40:12 +0530

    Delete 1849A_Morning_Sandwich.py

--- 1849A_Morning_Sandwich.py
+++
@@ -1,8 +0,0 @@
-for _ in range(int(input())):
-    a,b,c=map(int,input().split())
-    if(a>(b+c)):
-        print(2*(b+c)+1)
-    elif(a==(b+c)):
-        print(2*(b+c)-1)
-    else:
-        print(2*a-1)
